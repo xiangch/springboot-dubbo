b0029009720c3b6cbd175b1850d4ee55d6314659
曾祥超 <zengxiangchao@do1.com.cn>
2017-12-26 09:48:16 +0800

    优化配置。使用dubbo.properties

--- springboot-dubbo-provide/src/main/java/cn/com/do1/ProvideApp.java
+++ springboot-dubbo-provide/src/main/java/cn/com/do1/ProvideApp.java
@@ -3,11 +3,11 @@ package cn.com.do1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-
+@ImportResource({ "classpath:dubbo/*.xml" })
 @SpringBootApplication
-public class App {
+public class ProvideApp {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(ProvideApp.class, args);
         try {
             System.in.read();
         } catch (Exception e) {
